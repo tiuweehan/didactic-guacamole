d4fbde0fa36b8ec9cd96f65b5860afd10efd245f
Weehan <deantiu56@gmail.com>
2019-08-12 10:08:40 +0800

    Add Title and Navbar

--- frontend/src/components/layouts/Navbar.tsx
+++ frontend/src/components/layouts/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -11,11 +10,21 @@ const useStyles = makeStyles((theme: Theme) =>
     appBar: {
       backgroundColor: '#24292e',
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
     title: {
       flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+      color: '#9d9d9d',
+      marginLeft: theme.spacing(2),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      color: '#9d9d9d',
+      '&:hover': {
+        color: '#ddd',
+      },
     },
   }),
 );
@@ -26,7 +35,10 @@ const Navbar: React.FC = () => {
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar variant="dense" />
+        <Toolbar variant="dense">
+          <Typography className={classes.title}>Tiu Wee Han</Typography>
+          <Button className={classes.menuButton}>About</Button>
+        </Toolbar>
       </AppBar>
     </div>
   );
