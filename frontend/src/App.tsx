1576d5d1acbbf87bfe11e737554f9b0c9a193a1d
Weehan <deantiu56@gmail.com>
2019-08-12 11:48:29 +0800

    Add parallax background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { StylesProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
 import Layout from 'components/layouts/Layout';
+import Parallax from 'components/layouts/Parallax';
 
 const App: React.FC = () => {
   return (
-    <>
+    <StylesProvider injectFirst>
+      <CssBaseline />
       <Layout />
-    </>
+      <Parallax />
+    </StylesProvider>
   );
 };
 
---
+++ frontend/src/components/layouts/Parallax.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles(() =>
+  createStyles({
+    wrapper: {
+      height: '100vh',
+      overflowX: 'auto',
+      overflowY: 'auto',
+      perspective: '2px',
+    },
+    section: {
+      position: 'relative',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      textShadow: '0 0 5px #000',
+    },
+    parallax: {
+      '&:after': {
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        transform: 'translateZ(-2px) scale(2)',
+        backgroundSize: '100%',
+        zIndex: -1,
+      },
+    },
+    static: {
+      background: 'gray',
+    },
+    bg1: {
+      '&:after': {
+        backgroundImage: "url('/images/kitten.png')",
+      },
+    },
+  }),
+);
+
+const Parallax: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <main className={classes.wrapper}>
+      <section className={`${classes.section} ${classes.parallax} ${classes.bg1}`}>
+        <h1 style={{ fontSize: 50 }}>Tiu Wee Han</h1>
+      </section>
+      <section className={`${classes.section} ${classes.static}`}>
+        <h1>{'Welcome to my website!'}</h1>
+      </section>
+    </main>
+  );
+};
+
+export default Parallax;
