2c69287809f0566510460f245e970401a160f93f
Tiu Wee Han <deantiu56@gmail.com>
2020-01-06 16:10:26 +0800

    Add Resume

--- frontend/src/components/home/pages/HomeProfile.tsx
+++ frontend/src/components/home/pages/HomeProfile.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import LineBreak from 'components/layouts/utils/LineBreak';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -37,7 +38,17 @@ const HomeProfile: React.FC = () => {
         <a href="https://en.wikipedia.org/wiki/National_University_of_Singapore">National University of Singapore</a>.
       </Typography>
       <img className={classes.nusLogo} src="/images/NUS.png" alt="NUS" />
-      <Typography></Typography>
+      <LineBreak height={'50px'} />
+      <Typography>I started programming in August 2018 and my life has changed completely since then.</Typography>
+      <Typography>I enjoy learning and teaching, and also like doing DevOps related stuff.</Typography>
+      <LineBreak height={'100px'} />
+      <Typography>
+        Find my resume{' '}
+        <a href="/documents/Tiu_Wee_Han_resume.pdf" target="_blank">
+          here
+        </a>
+        .
+      </Typography>
     </div>
   );
 };
